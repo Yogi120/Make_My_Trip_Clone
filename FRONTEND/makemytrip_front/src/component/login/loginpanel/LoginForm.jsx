f15072306c7e5f67d3b7a7395eb23a468d45f236
Yogesh Pal <120707604+Yogi120@users.noreply.github.com>
2024-07-18 13:40:36 +0530

    Update LoginForm.jsx

--- FRONTEND/makemytrip_front/src/component/login/loginpanel/LoginForm.jsx
+++ FRONTEND/makemytrip_front/src/component/login/loginpanel/LoginForm.jsx
@@ -136,7 +136,7 @@ export const LoginForm = () => {
     e.preventDefault();
 
     axios
-      .post("https://make-my-trip-clone-backend.vercel.app/api/auth/Login", {
+      .post("https://make-my-trip-clone-omega.vercel.app/api/auth/Login", {
         email: value.email,
         password: value.password,
       })
